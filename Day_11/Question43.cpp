508d07ebcaaab1702586d6f5aacf4b890c97ca1f
Mayank Pandey <mayankpandey9346@gmail.com>
2026-07-01 16:24:25 +0530

    Day 11 uploaded

---
+++ Day_11/Question43.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int prime(int n)
+{
+    int i;
+
+    if(n<=1)
+        return 0;
+
+    for(i=2;i<n;i++)
+    {
+        if(n%i==0)
+            return 0;
+    }
+
+    return 1;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    if(prime(n))
+        cout<<"Prime";
+    else
+        cout<<"Not Prime";
+
+    return 0;
+}
